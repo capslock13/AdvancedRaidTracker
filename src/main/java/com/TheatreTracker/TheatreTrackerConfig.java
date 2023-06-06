45e62783e0acd3541596028fc97f0e716eb4db83
capslock13 <capslock13osrs@gmail.com>
2023-06-05 20:55:19 -0400

    name change

--- src/main/java/com/TheatreTracker/TheatreTrackerConfig.java
+++ src/main/java/com/TheatreTracker/TheatreTrackerConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("Theatre Tracker")
+@ConfigGroup("Theatre Statistic Tracker")
 
 public interface TheatreTrackerConfig extends Config
 {

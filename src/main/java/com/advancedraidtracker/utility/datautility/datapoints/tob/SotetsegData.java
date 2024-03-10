217d78a4894fa7264adaf3ee1177d4c60df1b4fd
Viktor <viktor.horsmanheimo@gmail.com>
2024-03-10 23:32:23 +0200

    Add skeleton for sotetseg

--- src/main/java/com/advancedraidtracker/utility/datautility/datapoints/tob/SotetsegData.java
+++ src/main/java/com/advancedraidtracker/utility/datautility/datapoints/tob/SotetsegData.java
@@ -1,11 +1,19 @@
 package com.advancedraidtracker.utility.datautility.datapoints.tob;
 
+import com.advancedraidtracker.utility.datautility.datapoints.LogEntry;
 import com.advancedraidtracker.utility.datautility.datapoints.RoomDataManager;
+
+import java.util.List;
 
 public class SotetsegData extends RoomDataManager
 {
-    public SotetsegData()
+    public SotetsegData(List<LogEntry> roomData)
     {
-        super(200, null);
+        super(200, roomData);
+    }
+
+    @Override
+    public String getName() {
+        return "Sotetseg";
     }
 }

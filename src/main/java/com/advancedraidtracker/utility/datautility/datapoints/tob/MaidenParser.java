02d9fdbab560fd9d7ac55c58b6df5dc9f6e83ccf
capslock13 <caps_lock13@live.com>
2024-04-13 01:01:50 -0400

    add split time to maiden lines

--- src/main/java/com/advancedraidtracker/utility/datautility/datapoints/tob/MaidenParser.java
+++ src/main/java/com/advancedraidtracker/utility/datautility/datapoints/tob/MaidenParser.java
@@ -1,6 +1,7 @@
 package com.advancedraidtracker.utility.datautility.datapoints.tob;
 
 import com.advancedraidtracker.constants.RaidRoom;
+import com.advancedraidtracker.utility.RoomUtil;
 import com.advancedraidtracker.utility.datautility.DataPoint;
 import com.advancedraidtracker.utility.datautility.datapoints.Raid;
 import com.advancedraidtracker.utility.datautility.datapoints.RoomParser;
@@ -24,10 +25,10 @@ public class MaidenParser extends RoomParser
     @Override
     public Map<Integer, String> getLines()
     {
-        lines.put(data.get(DataPoint.MAIDEN_70_SPLIT), "70s");
-        lines.put(data.get(DataPoint.MAIDEN_50_SPLIT), "50s");
-        lines.put(data.get(DataPoint.MAIDEN_30_SPLIT), "30s");
-        lines.put(data.get(DataPoint.MAIDEN_TIME), "Dead");
+        lines.put(data.get(DataPoint.MAIDEN_70_SPLIT), "70s - " + RoomUtil.time(data.get(DataPoint.MAIDEN_70_SPLIT)));
+        lines.put(data.get(DataPoint.MAIDEN_50_SPLIT), "50s - " + RoomUtil.time(data.get(DataPoint.MAIDEN_7050_DURATION)));
+        lines.put(data.get(DataPoint.MAIDEN_30_SPLIT), "30s - " + RoomUtil.time(data.get(DataPoint.MAIDEN_5030_SPLIT)));
+        lines.put(data.get(DataPoint.MAIDEN_TIME), "Dead - " + RoomUtil.time(data.get(DataPoint.MAIDEN_SKIP_SPLIT)));
         return lines;
     }
 }

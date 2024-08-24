763292e5d0ec7dd406a0dff4a7ab7cec41b3ff0a
capslock13 <caps_lock13@live.com>
2024-08-23 20:13:39 -0400

    ItemReference: Add "Dizana" as range keyword

--- src/main/java/com/advancedraidtracker/utility/ItemReference.java
+++ src/main/java/com/advancedraidtracker/utility/ItemReference.java
@@ -62,7 +62,8 @@ public class ItemReference
                     "darts",
                     "quiver",
                     "eclipse",
-                    "ralos"
+                    "ralos",
+					"dizana"
             },
             {
                     "ancestral",
